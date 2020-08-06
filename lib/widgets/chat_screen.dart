a9f000023d2def2834b22d4bc8c9c656f285c5d6
abdozdgaia <oldtimeprogrammer@yahoo.com>
2020-08-06 20:33:18 +0200

    last commit to the UI version

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -13,10 +13,18 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
+    final Container msg = Container(
+      width: MediaQuery.of(context).size.width * 0.75,
       margin: isMe
-          ? EdgeInsets.only(top: 8.0, bottom: 8.0, left: 80.0)
-          : EdgeInsets.only(top: 8.0, bottom: 8.0, right: 80.0),
+          ? EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 80.0,
+            )
+          : EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+            ),
       padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
       decoration: BoxDecoration(
         color: isMe ? Theme.of(context).accentColor : Color(0xffffefee),
@@ -36,6 +44,58 @@ class _ChatScreenState extends State<ChatScreen> {
           Text(message.time),
           SizedBox(height: 5.0),
           Text(message.text),
+        ],
+      ),
+    );
+
+    if (isMe) return msg;
+    return Row(
+      children: [
+        msg,
+        IconButton(
+          icon: Icon(
+            message.isLiked ? Icons.favorite : Icons.favorite_border,
+            size: 30.0,
+            color: message.isLiked
+                ? Theme.of(context).primaryColor
+                : Colors.blueGrey,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(
+              Icons.photo,
+              color: Theme.of(context).primaryColor,
+            ),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              onChanged: (value) {},
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.send,
+              color: Theme.of(context).primaryColor,
+            ),
+            onPressed: () {},
+          ),
         ],
       ),
     );
@@ -62,41 +122,47 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+      body: GestureDetector(
+        onTap: () {
+          return FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
                 ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(30.0),
-                  topLeft: Radius.circular(30.0),
-                ),
-                child: ListView.builder(
-                  reverse: true,
-                  padding: EdgeInsets.only(top: 15.0),
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    // final List<Message> messagesList = messages
-                    //     .where((i) =>
-                    //         i.sender.id == currentUser.id ||
-                    //         i.sender.id == widget.user.id)
-                    //     .defaultIfEmpty(Message());
-                    final message = messages[index];
-                    final bool isMe = message.sender.id == currentUser.id;
-                    return _buildMessage(message, isMe);
-                  },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      // final List<Message> messagesList = messages
+                      //     .where((i) =>
+                      //         i.sender.id == currentUser.id ||
+                      //         i.sender.id == widget.user.id)
+                      //     .defaultIfEmpty(Message());
+                      final message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
